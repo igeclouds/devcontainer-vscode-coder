151c23311f6a77342fa50eb73a74ecb8b78d3ac8
Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>
2023-06-21 06:29:06 -0700

    Update 01-CreateDatabase.sql
    Signed-off-by: Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>

--- 01-CreateDatabase.sql
+++ 01-CreateDatabase.sql
@@ -1,12 +1,12 @@
--- Create a new database called 'DatabaseName'
+-- Create a new database called appysqldb
 -- Connect to the 'master' database to run this snippet
 USE master
 GO
 -- Create the new database if it does not exist already
 IF NOT EXISTS (
-    SELECT name
+    SELECT appysqldb
         FROM sys.databases
-        WHERE name = N'TrySQLCMD'
+        WHERE name = N'appySQLCMD'
 )
-CREATE DATABASE TrySQLCMD
+CREATE DATABASE APPYSQLCMD
 GO
